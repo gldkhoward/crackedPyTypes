9f1a698c18c288ab11d7c72332ce5379636c9601
gldkhoward <ekulhoward@hotmail.com>
2023-09-06 18:53:20 +1000

    Update listExt.py
    Adding new functionality

--- listExt.py
+++ listExt.py
@@ -7,7 +7,6 @@ class listExt(list):
     """
     Class to extend the list type with some useful methods for data analytics.
     """
-
     def mean(self):
         '''
         Function to calculate the mean of a list.
@@ -33,10 +32,36 @@ class listExt(list):
         srtd = sorted(self)
         mid = len(self)//2
         return (srtd[mid] + srtd[~mid])/2      
+    
+    def greaterThan(self, value):
+        '''
+        Method to remove all values greater than a given value.
 
+        Parameters:
+            self (): list to be copied.
+            value (): value to be used as threshold.
+
+        Returns:
+            nArr (): new list with values greater than value removed.
+        '''
+        return [x for x in self if x > value]
+    
+    def lessThan(self, value):
+        '''
+        Method to remove all values less than a given value.
+
+        Parameters:
+            self (): list to be copied.
+            value (): value to be used as threshold.
+
+        Returns:
+            nArr (): new list with values less than value removed.
+        '''
+        return [x for x in self if x < value]
+    
     def N_minMax(self, rangeArr):
         '''
-        Function to normalize an array based a input array containing min and max values.
+       Method to normalize an array based a input array containing min and max values.
 
         If the input array is [0,1,2,3,4,5,6,7,8,9,10] and border array is [-1,1] the returned array would be
         [-1,-0.8,-0.6,-0.4,-0.2,0,0.2,0.4,0.6,0.8,1]
@@ -54,10 +79,9 @@ class listExt(list):
         aMin, aMax = np.amin(self), np.amax(self)
         return (self - aMin)*(newMax-newMin)/(aMax - aMin)+newMin
 
-    
     def N_Zscore(self):
         '''
-        Function to z an array based on z-score.
+        Method to normalize to z on array based on z-score.
 
         Parameters:
             self (): array to be normalized.
